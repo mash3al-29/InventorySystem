335e3a3c060d91e682b810442fb8e13fe2a0033f
Abourida <omarabourida2015@gmail.com>
2023-11-07 21:43:23 +0200

    Added Omar is admin

--- src/main/java/com/lab4/inventoryproject/inventoryproject/AdminRole.java
+++ src/main/java/com/lab4/inventoryproject/inventoryproject/AdminRole.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//Omar is admin
 package com.lab4.inventoryproject.inventoryproject;
 
 import java.util.ArrayList;
